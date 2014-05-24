2862bbae96db849444e35c3636adb12ae881b0d5
Luc Saffre <luc.saffre@gmail.com>
2014-05-24 06:54:33 +0300

    http://docs.lino-framework.org/blog/2014/0524.html

--- lino_cosi/settings/demo.py
+++ lino_cosi/settings/demo.py
@@ -1,4 +1,7 @@
 from lino_cosi.settings import *
-SITE = Site(globals(), title="Lino-Cosi demo", is_demo_site=True)
+ad.configure_plugin('contacts', hide_region=True)
+SITE = Site(globals(), title="Lino Cosi demo", is_demo_site=True)
+
+
 # the following line should not be active in a checked-in version
 #~ DATABASES['default']['NAME'] = ':memory:'
