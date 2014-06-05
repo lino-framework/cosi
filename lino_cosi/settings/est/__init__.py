7b658e368b9299797b62104ce1372af8a9c7927e
Luc Saffre <luc.saffre@gmail.com>
2014-06-05 04:51:08 +0300

    http://docs.lino-framework.org/blog/2014/0605.html

---
+++ lino_cosi/settings/est/__init__.py
@@ -0,0 +1,47 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2014 Luc Saffre
+# This file is part of the Lino project.
+# Lino is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# Lino is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with Lino; if not, see <http://www.gnu.org/licenses/>.
+
+"""
+Default settings for an Estonian :ref:`cosi` site.
+
+- `Estonian VAT rates
+  <http://www.emta.ee/index.php?id=28460>`_
+
+
+"""
+
+from __future__ import unicode_literals
+
+from decimal import Decimal
+
+from lino_cosi.settings import *
+
+from django.utils.translation import ugettext_lazy as _
+
+
+class Site(Site):
+    title = "Lino Cosi Eesti"
+    languages = 'en et'
+    demo_fixtures = 'std few_languages furniture \
+    demo ee demo2'.split()
+
+    def setup_plugins(self):
+        self.plugins.contacts.configure(hide_region=False)
+        self.plugins.ledger.configure(use_pcmn=True)
+        self.plugins.vat.VAT_CLASS_TO_RATE.update(
+            reduced=Decimal('0.09'),
+            normal=Decimal('0.20'))
+
+        super(Site, self).setup_plugins()
+
---
+++ lino_cosi/settings/est/demo.py
@@ -0,0 +1,12 @@
+from lino_cosi.settings.est import *
+
+
+class Site(Site):
+    title = Site.title + ' demo'
+    is_demo_site = True
+
+SITE = Site(globals())
+
+
+# the following line should not be active in a checked-in version
+#~ DATABASES['default']['NAME'] = ':memory:'
