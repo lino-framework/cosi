62a88f94e7735a6588796667d4f06fc8d51e72c9
Luc Saffre <luc.saffre@gmail.com>
2014-06-14 23:16:37 +0300

    http://docs.lino-framework.org/blog/2014/0614.html

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,7 @@ setup_from_project('lino_cosi', 'lino_cosi.settings.demo')
 
 env.demo_databases.append('lino_cosi.settings.est.demo')
 env.demo_databases.append('lino_cosi.settings.be.demo')
+env.demo_databases.append('lino_cosi.settings.start.demo')
 
 env.languages = "en de fr et nl es".split()
 
