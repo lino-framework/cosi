75087fff142a8fae3f44854a5e8dca7b26f57321
Luc Saffre <luc.saffre@gmail.com>
2014-09-11 06:29:44 +0300

    http://docs.lino-framework.org/blog/2014/0911.html

--- lino_cosi/project_info.py
+++ lino_cosi/project_info.py
@@ -26,7 +26,7 @@ application to make accounting simple.
     author='Luc Saffre',
     author_email='luc.saffre@gmail.com',
     url="http://cosi.lino-framework.org",
-    license='GPL',
+    license='LGPL',
     classifiers="""\
 Programming Language :: Python
 Programming Language :: Python :: 2
@@ -35,7 +35,7 @@ Environment :: Web Environment
 Framework :: Django
 Intended Audience :: Developers
 Intended Audience :: System Administrators
-License :: OSI Approved :: GNU General Public License (GPL)
+License :: OSI Approved :: GNU Lesser General Public License (LGPL)
 Operating System :: OS Independent
 Topic :: Office/Business :: Scheduling
 """.splitlines())
--- lino_cosi/settings/start/doctests.py
+++ lino_cosi/settings/start/doctests.py
@@ -1,3 +1,3 @@
-from ..start import *
+from lino_cosi.settings import *
 SITE = Site(globals(), no_local=True, remote_user_header='REMOTE_USER')
 DEBUG = True
