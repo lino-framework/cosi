2d7a0f60ea95462e9d598b27efb634b12574f620
Luc Saffre <luc.saffre@gmail.com>
2014-09-12 10:12:41 +0300

    http://docs.lino-framework.org/blog/2014/0912.html

--- lino_cosi/models.py
+++ lino_cosi/models.py
@@ -2,31 +2,20 @@
 # Copyright 2011-2014 Luc Saffre
 # This file is part of the Lino project.
 # Lino is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 # Lino is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
+# GNU Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public License
 # along with Lino; if not, see <http://www.gnu.org/licenses/>.
 
 """
 The :xfile:`models.py` module for :ref:`cosi`.
 """
 
-import os
-import cgi
-import datetime
-
-from django.db import models
-#~ from django.db.models import Q
-from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
-
-
-#~ from lino import mixins
 from lino import dd
 
 
@@ -45,11 +34,6 @@ def site_setup(site):
         seqno chart group type clearable
         ledger.MovementsByAccount
         """)
-
-
-# @dd.when_prepared('contacts.Person', 'contacts.Company')
-# def hide_region(model):
-#     model.hide_elements('region')
 
 
 @dd.receiver(dd.pre_analyze)
--- lino_cosi/settings/start/settings.py
+++ lino_cosi/settings/start/settings.py
@@ -12,10 +12,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Lino; if not, see <http://www.gnu.org/licenses/>.
 
-"""
-This currently depends on :ref:`cosi` project.
-"""
-
 from __future__ import unicode_literals
 
 from lino_cosi.settings import *
