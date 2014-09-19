4f88bb301603e6e808dfc9f463691f75c2f4d7b9
Luc Saffre <luc.saffre@gmail.com>
2014-09-19 05:51:39 +0300

    http://docs.lino-framework.org/blog/2014/0919.html

--- lino_cosi/setup_info.py
+++ lino_cosi/setup_info.py
@@ -1,16 +1,6 @@
 # -*- coding: UTF-8 -*-
 # Copyright 2014 Luc Saffre
-# This file is part of the Lino project.
-# Lino is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-# Lino is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-# You should have received a copy of the GNU Lesser General Public License
-# along with Lino; if not, see <http://www.gnu.org/licenses/>.
+# License: BSD (see file COPYING for details)
 
 SETUP_INFO = dict(
     name='lino-cosi',
@@ -27,7 +17,7 @@ make accounting simple.
     author='Luc Saffre',
     author_email='luc.saffre@gmail.com',
     url="http://cosi.lino-framework.org",
-    license='LGPL',
+    license='BSD License',
     classifiers="""\
 Programming Language :: Python
 Programming Language :: Python :: 2
@@ -36,7 +26,7 @@ Environment :: Web Environment
 Framework :: Django
 Intended Audience :: Developers
 Intended Audience :: System Administrators
-License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
+License :: OSI Approved :: BSD License
 Operating System :: OS Independent
 Topic :: Office/Business :: Scheduling
 """.splitlines())
