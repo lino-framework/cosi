ada114d0f42a8405214ec2939163c97f1a3279a9
Luc Saffre <luc.saffre@gmail.com>
2016-08-03 17:19:09 +0300

    Py3 execfile

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
-execfile('lino_cosi/setup_info.py')
+fn = 'lino_cosi/setup_info.py'
+exec(compile(open(fn, "rb").read(), fn, 'exec'))
 if __name__ == '__main__':
     setup(**SETUP_INFO)
