db4b2c44868a9d87ae491dd5a8afa1e6a524793c
Hamza Khchine <hamzakhchine@gmail.com>
2016-08-05 07:14:50 +0100

    Clear setup.py file

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 fn = 'lino_cosi/setup_info.py'
 exec(compile(open(fn, "rb").read(), fn, 'exec'))
-# above line is equivalent to the line below, except that it works
-# also in Python 3:
-# execfile(fn)
 if __name__ == '__main__':
     setup(**SETUP_INFO)
