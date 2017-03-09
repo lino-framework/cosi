1acc4400db1e02eacfe9506d829fa85163222dc0
Luc Saffre <luc.saffre@gmail.com>
2017-03-09 14:22:21 +0200

    http://luc.lino-framework.org/blog/2017/0309.html

--- lino_cosi/projects/std/settings/demo.py
+++ lino_cosi/projects/std/settings/demo.py
@@ -12,13 +12,14 @@ class Site(DocsSite):
     the_demo_date = datetime.date(2015, 05, 12)
     languages = "en fr de"
 
-    def setup_plugins(self):
-        """
-        Change the default value of certain plugin settings.
+    # def setup_plugins(self):
+    #     """
+    #     Change the default value of certain plugin settings.
 
-        """
-        super(Site, self).setup_plugins()
-        self.plugins.ledger.configure(start_year=2015)
+    #     """
+    #     super(Site, self).setup_plugins()
+    #     self.plugins.ledger.configure(start_year=2015)
 
 SITE = Site(globals())
 DEBUG = True
+SITE.plugins.ledger.configure(start_year=2015)
