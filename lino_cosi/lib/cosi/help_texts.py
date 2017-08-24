db6337c4d355caa9fa3ec1c447a1b3ae42f23665
Luc Saffre <luc.saffre@gmail.com>
2017-08-24 14:43:31 +0300

    http://luc.lino-framework.org/blog/2017/0824.html

--- lino_cosi/lib/cosi/help_texts.py
+++ lino_cosi/lib/cosi/help_texts.py
@@ -11,10 +11,10 @@ after it get imported."""),
     'lino_cosi.lib.b2c.camt.BankTransaction' : _("""Single transaction that is part of a bank statement."""),
     'lino_cosi.lib.b2c.camt.BankStatement' : _("""A bank statement groups data about several bank transactions."""),
     'lino_cosi.lib.b2c.camt.CamtParser' : _("""Parser for camt bank statement import files."""),
-    'lino_cosi.lib.b2c.models.ImportStatements' : _("""Import the .xml files found in the directory specified at
+    'lino_cosi.lib.b2c.ImportStatements' : _("""Import the .xml files found in the directory specified at
 import_statements_path."""),
-    'lino_cosi.lib.b2c.models.Account' : _("""A bank account related to a given Partner."""),
-    'lino_cosi.lib.b2c.models.Account.account_name' : _("""Name of the account, as assigned by the account servicing
+    'lino_cosi.lib.b2c.Account' : _("""A bank account related to a given Partner."""),
+    'lino_cosi.lib.b2c.Account.account_name' : _("""Name of the account, as assigned by the account servicing
 institution, in agreement with the account owner in order to
 provide an additional means of identification of the account.
 Usage: The account name is different from the
@@ -22,33 +22,33 @@ owner_name. The account name is used in certain user
 communities to provide a means of identifying the account, in
 addition to the account owner's identity and the account
 number."""),
-    'lino_cosi.lib.b2c.models.Account.owner_name' : _("""Name by which a party is known and which is usually used to
+    'lino_cosi.lib.b2c.Account.owner_name' : _("""Name by which a party is known and which is usually used to
 identify that party."""),
-    'lino_cosi.lib.b2c.models.Statement' : _("""A bank statement."""),
-    'lino_cosi.lib.b2c.models.Statement.sequence_number' : _("""The legal sequential number of the statement, as assigned by
+    'lino_cosi.lib.b2c.Statement' : _("""A bank statement."""),
+    'lino_cosi.lib.b2c.Statement.sequence_number' : _("""The legal sequential number of the statement, as assigned by
 the bank."""),
-    'lino_cosi.lib.b2c.models.Statement.end_date' : _("""Note that year can differ between start_date and end_date for
+    'lino_cosi.lib.b2c.Statement.end_date' : _("""Note that year can differ between start_date and end_date for
 the first statement of every year."""),
-    'lino_cosi.lib.b2c.models.Statement.unique_id' : _("""A virtual field of the form YYYY/NNNN where YYYY is taken
+    'lino_cosi.lib.b2c.Statement.unique_id' : _("""A virtual field of the form YYYY/NNNN where YYYY is taken
 from the end_date and NNNN is taken from
 electronic_sequence_number."""),
-    'lino_cosi.lib.b2c.models.Transaction' : _("""A transaction within a bank statement."""),
-    'lino_cosi.lib.b2c.models.Transaction.transfer_type' : _("""The actual historic name of the txcd."""),
-    'lino_cosi.lib.b2c.models.Transaction.txcd' : _("""The Bank Transaction Code (<BkTxCd>) or "transfer type".
+    'lino_cosi.lib.b2c.Transaction' : _("""A transaction within a bank statement."""),
+    'lino_cosi.lib.b2c.Transaction.transfer_type' : _("""The actual historic name of the txcd."""),
+    'lino_cosi.lib.b2c.Transaction.txcd' : _("""The Bank Transaction Code (<BkTxCd>) or "transfer type".
 Actually it is the "proprietary" part of this code."""),
-    'lino_cosi.lib.b2c.models.Transaction.txcd_issuer' : _("""The issuer or the txcd."""),
-    'lino_cosi.lib.b2c.models.Transaction.txcd_text' : _("""Virtual field with the textual translated description of the
+    'lino_cosi.lib.b2c.Transaction.txcd_issuer' : _("""The issuer or the txcd."""),
+    'lino_cosi.lib.b2c.Transaction.txcd_text' : _("""Virtual field with the textual translated description of the
 txcd.  Currently this works only for Belgian codes
 where txcd_issuer is "BBA" as defined in
 lino_cosi.lib.b2c.febelfin)."""),
-    'lino_cosi.lib.contacts.models.Partner' : _("""An version of lino_xl.lib.contacts.models.Partner which
+    'lino_cosi.lib.contacts.Partner' : _("""An version of lino_xl.lib.contacts.models.Partner which
 adds accounting fucntionality."""),
-    'lino_cosi.lib.contacts.models.Person' : _("""An version of lino_xl.lib.contacts.models.Person which
+    'lino_cosi.lib.contacts.Person' : _("""An version of lino_xl.lib.contacts.models.Person which
 adds accounting fucntionality."""),
-    'lino_cosi.lib.contacts.models.Company' : _("""An version of lino_xl.lib.contacts.models.Company which
+    'lino_cosi.lib.contacts.Company' : _("""An version of lino_xl.lib.contacts.models.Company which
 adds accounting fucntionality."""),
     'lino_cosi.lib.delivery.Plugin' : _("""See lino.core.plugin.Plugin."""),
-    'lino_cosi.lib.delivery.models.ShippingMode' : _("""Represents a possible method of how the items described in a
+    'lino_cosi.lib.delivery.ShippingMode' : _("""Represents a possible method of how the items described in a
 SalesDocument are to be transferred from us to our customer."""),
     'lino_cosi.lib.orders.Plugin' : _("""See lino.core.plugin.Plugin."""),
 }
