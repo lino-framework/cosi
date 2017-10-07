eedcb2ca1964fef522361df167fcbd9e6c0b3a7f
Luc Saffre <luc.saffre@gmail.com>
2017-10-07 20:43:37 +0300

    http://luc.lino-framework.org/blog/2017/1007.html

--- lino_cosi/lib/cosi/layouts.py
+++ lino_cosi/lib/cosi/layouts.py
@@ -41,7 +41,7 @@ PlacesByPlace contacts.PartnersByCity
 rt.models.accounts.Accounts.detail_layout = """
 ref:10 name
 group type id default_amount:10 vat_column
-needs_partner clearable purchases_allowed  needs_ana ana_account
+needs_partner clearable purchases_allowed
 ledger.MovementsByAccount
 """
 
@@ -50,7 +50,5 @@ site_company next_partner_id:10
 default_build_method simulate_today
 clients_account   sales_account
 suppliers_account purchases_account tax_offices_account
-site_calendar default_event_type #pupil_guestrole
-max_auto_events hide_events_before
 """
 
