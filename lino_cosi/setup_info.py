7a803913e37ad1d0d917c98b409618d62ada14fa
khchine5 <hamzakhchine@gmail.com>
2018-12-25 15:06:55 +0100

    #2765: Prepare to release 18.12.1

--- lino_cosi/setup_info.py
+++ lino_cosi/setup_info.py
@@ -4,8 +4,7 @@
 
 SETUP_INFO = dict(
     name='lino-cosi',
-    # version='0.0.4',
-    version='18.12.0',
+    version='18.12.1',
     install_requires=['lino-xl','django-iban', 'lxml'],
     tests_require=['beautifulsoup4',],
     test_suite='tests',
