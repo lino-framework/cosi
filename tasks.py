f3d73c05032ac5b1162728bf52ab6310fa727363
Luc Saffre <luc.saffre@gmail.com>
2021-03-03 21:19:53 +0200

    add support for language zh-hant

--- tasks.py
+++ tasks.py
@@ -1,10 +1,8 @@
 from atelier.invlib import setup_from_tasks
 ns = setup_from_tasks(
     globals(), "lino_cosi",
-    languages="en de fr et nl es".split(),
+    languages="en de fr et nl es zh-hant".split(),
     # tolerate_sphinx_warnings=True,
-    blogref_url="http://luc.lino-framework.org",
+    blogref_url="https://luc.lino-framework.org",
     locale_dir='lino_cosi/lib/cosi/locale',
     revision_control_system='git')
-
-
